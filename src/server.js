bfe7066cb593519611ac3b93fbf1d6ed40161de4
Sherin Binu <sherinbinu@hotmail.com>
2019-03-17 14:44:39 +0530

    add nodemon and dev script

--- src/server.js
+++ src/server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const pg = require('./db/pg');
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(5000, () => {
+  console.log('Listening on port 5000');
+});
