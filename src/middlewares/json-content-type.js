60253e5278fa3ed320cc534152e0a83a7c61a579
Sherin Binu <sherinbinu@hotmail.com>
2019-03-21 05:59:26 +0530

    use ResponseBody in middlewares

--- src/middlewares/json-content-type.js
+++ src/middlewares/json-content-type.js
@@ -1,10 +1,14 @@
+const ResponseBody = require("../helpers/classes/ResponseBody");
+
 module.exports = (req, res, next) => {
   if (!req.headers["content-type"].includes("application/json")) {
-    return res.status(415).json({
-      error: true,
-      code: "910",
-      message: 'The "Content-Type" header must always be "application/json"'
-    });
+    const rBody = new ResponseBody();
+    rBody.setMessage(
+      'The "Content-Type" header must always be "application/json"'
+    );
+    rBody.removePayload();
+    rBody.removePayload();
+    return res.status(415).json(rBody);
   }
   next();
 };
--- src/middlewares/set-content-type.js
+++ src/middlewares/set-content-type.js
@@ -1,18 +1,17 @@
+const ResponseBody = require("../helpers/classes/ResponseBody");
+
 module.exports = (req, res, next) => {
   if (
     req.headers["content-length"] &&
     req.headers["content-length"] !== "0" &&
     !req.headers["content-type"]
   ) {
-    return res
-      .status(400)
-      .set("Content-Type", "application/json")
-      .json({
-        error: true,
-        code: "920",
-        message:
-          'The "Content-Type" header must be set for requests with a non-empty payload'
-      });
+    const rBody = new ResponseBody();
+    rBody.setMessage(
+      'The "Content-Type" header must be set for requests with a non-empty payload'
+    );
+    rBody.removePayload();
+    return res.status(400).json(rBody);
   }
   next();
 };
