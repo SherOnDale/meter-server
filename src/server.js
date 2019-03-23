d6953b3eaa2735275d046bc67d63eef3d68f5eb5
Sherin Binu <sherinbinu@hotmail.com>
2019-03-23 20:13:56 +0530

    add docs

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const helmet = require("helmet");
-const logger = require("morgan");
-const userRoutes = require("./routes/user.route");
-const authRoutes = require("./routes/auth.route");
-const middlewares = require("./middlewares/index");
+const express = require('express');
+const fs = require('fs');
+const helmet = require('helmet');
+const logger = require('morgan');
+const userRoutes = require('./routes/user.route');
+const authRoutes = require('./routes/auth.route');
+const middlewares = require('./middlewares/index');
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(middlewares.handleEmptyPayload);
 app.use(middlewares.contentTypeSet);
@@ -17,9 +18,22 @@ app.use(middlewares.contentTypeJson);
 app.use(middlewares.handleErrors);
 app.use(middlewares.setResponseHeaders);
 
-app.use("/users", userRoutes);
-app.use("/auth", authRoutes);
+app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
+
+app.get('/meter.yaml', (req, res, next) => {
+  fs.readFile(`${__dirname}/docs/meter.yaml`, (err, file) => {
+    if (err) {
+      res.status(500);
+      res.end();
+      return next();
+    }
+    res.write(file);
+    res.end();
+    return next();
+  });
+});
 
 app.listen(5000, () => {
-  console.log("Listening on port 5000");
+  console.log('Listening on port 5000');
 });
