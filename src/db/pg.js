c48a1b4fcc3d0fb4930ead720e319db5b91ff757
Sherin Binu <sherinbinu@hotmail.com>
2019-03-30 07:12:38 +0530

    fix pg schema

--- src/db/pg.js
+++ src/db/pg.js
@@ -10,7 +10,7 @@ const pg = new Pool({
 
 pg.on('error', () => console.log('Lost PG connection'));
 pg.query(
-  "CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, email VARCHAR(30) UNIQUE, hash VARCHAR(50) DEFAULT 'not-set', email_salt VARCHAR(50), hash_salt VARCHAR(50) DEFAULT 'not-set', first_name VARCHAR(20) DEFAULT 'not-set', last_name VARCHAR(20) DEFAULT 'not-set', avatar_url VARCHAR(40) DEFAULT 'not-set', email_hash VARCHAR(50), referrer VARCHAR(30) DEFAULT 'not-set', verified BOOLEAN DEFAULT FALSE, profile BOOLEAN DEFAULT FALSE)"
+  'CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, email VARCHAR(30) UNIQUE, hash VARCHAR(50) DEFAULT NULL, email_salt VARCHAR(50), hash_salt VARCHAR(50) DEFAULT NULL, first_name VARCHAR(20) DEFAULT NULL, last_name VARCHAR(20) DEFAULT NULL, avatar_url VARCHAR(40) DEFAULT NULL, email_hash VARCHAR(50), referrer VARCHAR(30) DEFAULT NULL, verified BOOLEAN DEFAULT FALSE, profile BOOLEAN DEFAULT FALSE)'
 ).catch(error => console.log(error));
 
 module.exports = pg;
